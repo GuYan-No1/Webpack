095285054ab1d8f4225e05678208416ce674e7c2
wc437380 <wb-wc437380@alibaba-inc.com>
2019-01-17 18:59:20 +0800

    chore:study

---
+++ history/webpack.config.js
@@ -0,0 +1,30 @@
+//webpack 是node写出来的 node的写法
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports={
+    devServer: { //开发服务的配置
+        port: 3000, //端口号
+        progress: true, //进度条,
+        contentBase: './bulid', //指定服务指向的文件夹
+        open: true, //自动打开浏览器
+        compress: true, //启用gzip压缩
+    },
+    mode:'development',//模式 默认两种 production development
+    entry:'./src/index.js',//入口
+    output:{
+        filename:'bundle.[hash:8].js',//打包后的文件名
+        path:path.resolve(__dirname,'bulid'),//路径必须是一个绝对路径
+    },
+    plugins:[//放在所有的webpack插件
+        new HtmlWebpackPlugin({
+            templete:'./src/index.html',//传入模板的路径
+            filename:'index.html',//打包后的文件名
+            minify:{//压缩配置
+                removeAttributeQuotes:true,//删除属性的双引号
+                collapseWhitespace:true//折叠空行
+            },
+            hash:true//加入哈希戳引入
+        })
+
+    ]
+}
---
+++ note.md
@@ -0,0 +1,20 @@
+## webpack安装
+- 安装本地的webpack
+- webpack webpack-cli -D
+
+## webpack可以进行0配置
+- 打包工具 -> 输出的结果（js模块）
+- 打包(支持我们的js的模块化)
+
+## 手动配置webpack
+- 默认配置文件的名字 webpack.config.js
+
+## webpack-dev-server（webpack开发服务）
+- yarn add webpack-dev-server -D
+
+## HTML插件 
+- yarn add html-webpack-plugin -D
+
+## loader
+- yarn add css-loader -D
+- yarn add style-loader -D
